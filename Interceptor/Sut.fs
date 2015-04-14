fb243c9cc37c8e431e8529c173a9c53d50cfe84d
Vitaly Kamiansky <vitaly.kamiansky@firstlinesoftware.com>
2015-04-14 21:18:39 +0300

    Firefox part 2

--- Interceptor/Sut.fs
+++ Interceptor/Sut.fs
@@ -1,25 +1,27 @@
 ﻿namespace FirstLine
 open System
 open System.Threading
-open OpenQA.Selenium
-open OpenQA.Selenium.PhantomJS
+open OpenQA.Selenium.Firefox
 
 module Sut =
 
     let runScenario doPageOpenStart doPageOpenReady doTimeout doSlideshowClick urls =
-        let driverService = PhantomJSDriverService.CreateDefaultService(HideCommandPromptWindow = true)
-        let driver = new PhantomJSDriver(driverService)  
-        driver.Manage().Timeouts().SetPageLoadTimeout(TimeSpan.FromSeconds(10.)) |>ignore
+        let driver = ref (new FirefoxDriver()) 
+
         let runPageScenario (url:string) =
             try
                 doPageOpenStart url
-                driver.Navigate().GoToUrl(url)
+                (!driver).Navigate().GoToUrl(url)
+                Thread.Sleep(2000)
                 doPageOpenReady url
-                match driver.FindElementsByClassName("b-slideshow__next") |> List.ofSeq with
-                    | slideshowNext :: _ -> doSlideshowClick (); slideshowNext.Click()
+                match (!driver).FindElementsByClassName("b-slideshow__next") |> List.ofSeq with
+                    | slideshowNext :: _ -> doSlideshowClick (); slideshowNext.Click(); Thread.Sleep(2000) 
+                    | [] -> ()
+                match (!driver).FindElementsByClassName("sb-player") |> List.ofSeq with
+                    | video :: _ -> if video.Displayed then Thread.Sleep(90000)  
                     | [] -> ()
             with
-                | :? WebDriverTimeoutException -> doTimeout ()
+                | _-> (!driver).Quit(); (!driver).Dispose(); driver := new FirefoxDriver()
         urls |> List.iter runPageScenario
-        driver.Quit()
-        driver.Dispose()
+        (!driver).Quit()
+        (!driver).Dispose()
