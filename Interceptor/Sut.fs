5cdc7805d17240735a90ff3ce7f95db342a47534
Vitaly Kamiansky <vitaly.kamiansky@firstlinesoftware.com>
2015-04-15 13:11:48 +0300

    tab clicks on start page

--- Interceptor/Sut.fs
+++ Interceptor/Sut.fs
@@ -5,7 +5,7 @@ open OpenQA.Selenium.Firefox
 
 module Sut =
 
-    let runScenario doPageOpenStart doPageOpenReady doTimeout doSlideshowClick urls =
+    let runScenario doPageOpenStart doPageOpenReady doTimeout doSlideshowClick doTabClick urls =
         let driver = ref (new FirefoxDriver()) 
 
         let runPageScenario (url:string) =
@@ -17,11 +17,18 @@ module Sut =
                 match (!driver).FindElementsByClassName("b-slideshow__next") |> List.ofSeq with
                     | slideshowNext :: _ -> doSlideshowClick (); slideshowNext.Click(); Thread.Sleep(2000) 
                     | [] -> ()
+                match (!driver).FindElementsByClassName("pageGuid") |> List.ofSeq with
+                    | guid :: _ -> 
+                    match guid.GetAttribute("value"), (!driver).FindElementsByClassName("tab-link") |> List.ofSeq with
+                        | "0bf95c48-b2c4-43d1-8c28-8a02abaf0d38", tabs when tabs.Length > 0 -> 
+                              tabs |> List.iter (fun t -> doTabClick(t.Text); t.Click(); Thread.Sleep(1000) )
+                        | _ -> ()                    
+                    | [] -> ()
                 match (!driver).FindElementsByClassName("sb-player") |> List.ofSeq with
                     | video :: _ -> if video.Displayed then Thread.Sleep(90000)  
                     | [] -> ()
             with
-                | _-> (!driver).Quit(); (!driver).Dispose(); driver := new FirefoxDriver()
+                | ex -> (!driver).Quit(); (!driver).Dispose(); driver := new FirefoxDriver(); 
         urls |> List.iter runPageScenario
         (!driver).Quit()
         (!driver).Dispose()
