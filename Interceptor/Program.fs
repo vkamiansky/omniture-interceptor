b596f1a90d0fbbfd0676be21f3e55f68d18ef091
Vitaly Kamiansky <vitaly.kamiansky@firstlinesoftware.com>
2015-04-16 16:34:14 +0300

    urls list fragmentation

--- Interceptor/Program.fs
+++ Interceptor/Program.fs
@@ -8,6 +8,21 @@ module interceptor =
     let writeIfNotEmpty str = 
         if str |> (String.IsNullOrEmpty >> not) then printf "%s\r\n" str
 
+    let rec skip n (lst : 'a list) =
+        match lst with
+        | _::tail -> if n <= 1 then tail else tail |> skip(n-1)
+        | _ -> []
+
+    let rec take n (lst : 'a list) =
+        match lst with
+        | head::tail -> if n > 0 then head :: (tail |> take(n-1)) else []
+        | _ -> []
+
+    let rec inBatchesOfN n (lst : 'a list) =
+        match lst with
+        | _::_ -> (lst |> take(n)):: inBatchesOfN n (lst |> skip(n))
+        | _ -> []
+
     [<EntryPoint>]
     let main argv = 
         let on = ref true    
@@ -19,15 +34,17 @@ module interceptor =
         let candidateBase = System.Configuration.ConfigurationManager.AppSettings.["candidate"]
         let relAddresses = List.ofSeq (System.IO.File.ReadLines("testpath.txt"))
         let addressPairs = relAddresses |> List.map (fun adr -> currentBase + adr, candidateBase + adr)
+        let addresses = addressPairs |> List.collect (fun (cur, pro) -> [cur; pro])
 
         printf "To start the test press a key...\r\n"
         Console.ReadKey() |>  ignore
+
         Omniture.intercept (fun () -> !on) (fun ()-> data) (fun req-> requests := !requests |> List.append [req]; printf "Caught one. Now they are %d.\r\n" (!requests).Length;)
      
         printf "Carrying out test scenario. %d pages will be opened each in 2 environments, requests intercepted.\r\n" (relAddresses.Length)
-        addressPairs
-        |> List.collect (fun (cur, pro) -> [cur; pro])
-        |> Scenario.run (fun adr -> printf "Starting '%s'\r\n" adr) (fun adr -> printf " -Ready\r\n") (fun () -> printf " -Page load timeout\r\n") (fun () -> printf " -Slideshow click\r\n") (fun t -> printf " -\"%s\" Tab click\r\n" t) (fun () -> printf " -Tab show more click\r\n") (fun () -> printf " -Tab senaste dygnet click\r\n")  (fun l -> printf " -\"%s\" Tab link click\r\n" l) 
+        addresses
+        |> inBatchesOfN 4
+        |> List.iter (fun l -> l |> Scenario.run (fun adr -> printf "Starting '%s'\r\n" adr) (fun adr -> printf " -Ready\r\n") (fun () -> printf " -Page load timeout\r\n") (fun () -> printf " -Slideshow click\r\n") (fun t -> printf " -\"%s\" Tab click\r\n" t) (fun () -> printf " -Tab show more click\r\n") (fun () -> printf " -Tab senaste dygnet click\r\n")  (fun l -> printf " -\"%s\" Tab link click\r\n" l)) 
 
         printf "Test scenario finished. To stop capturing press a key...\r\n"
         Console.ReadKey() |>  ignore
--- Interceptor/Scenario.fs
+++ Interceptor/Scenario.fs
@@ -1,46 +1,64 @@
 ﻿namespace FirstLine
 open System
+open System.Diagnostics
 open System.Threading
-open OpenQA.Selenium
+open OpenQA.Selenium.Remote
 open OpenQA.Selenium.Firefox
 
 module Scenario =
+   
+    let driverGet () =
+        new FirefoxDriver()
+
+    let driverBrowse (url:string) doPageOpenStart doPageOpenReady (driver:RemoteWebDriver) =
+        doPageOpenStart url
+        driver.Navigate().GoToUrl(url)
+        Thread.Sleep(1000)
+        doPageOpenReady url
+    
+    let driverFindSelector sel (driver:RemoteWebDriver) =
+        sel |> driver.FindElementsByCssSelector |> List.ofSeq
+
+    let driverCleanup (driver:RemoteWebDriver)  =
+        driver.Quit()
+        driver.Dispose()
 
     let run doPageOpenStart doPageOpenReady doTimeout doSlideshowClick doTabClick doTabShowMoreClick doTabReadMoreClick doTabLinkClick urls =
-        let driver = ref (new FirefoxDriver()) 
+        let driver = ref (driverGet())
+        let browse url = !driver |> driverBrowse url doPageOpenStart doPageOpenReady
+        let findSel sel = !driver |> driverFindSelector sel
+        let cleanup() = !driver |> driverCleanup
+        let reset() = driver := driverGet()
 
         let runPageScenario (url:string) =
             try
-                doPageOpenStart url
-                (!driver).Navigate().GoToUrl(url)
-                Thread.Sleep(2000)
-                doPageOpenReady url
-                match (!driver).FindElementsByClassName("b-slideshow__next") |> List.ofSeq with
-                    | slideshowNext :: _ -> doSlideshowClick (); slideshowNext.Click(); Thread.Sleep(2000) 
+                browse url 
+                match findSel ".b-slideshow__next" with
+                    | slideshowNext :: _ -> doSlideshowClick (); slideshowNext.Click(); Thread.Sleep(1000) 
                     | [] -> ()
-                match (!driver).FindElementsByClassName("pageGuid") |> List.ofSeq with
+                match findSel ".pageGuid" with
                     | guid :: _ -> 
-                    match guid.GetAttribute("value"), (!driver).FindElementsByClassName("tab-link") |> List.ofSeq with
+                    match guid.GetAttribute("value"), findSel ".tab-link" with
                         | "0bf95c48-b2c4-43d1-8c28-8a02abaf0d38", tabs when tabs.Length > 0 ->
-                              match (!driver).FindElementsByCssSelector(".b-tabs__tab-content_active .b-link__show-more") |> List.ofSeq with
+                              match findSel ".b-tabs__tab-content_active .b-link__show-more" with
                                   | showMore :: _ -> doTabShowMoreClick(); showMore.Click(); Thread.Sleep(1000)
                                   | [] -> () 
                               tabs |> List.iter (fun t -> doTabClick(t.Text); t.Click(); Thread.Sleep(1000))
-                              doPageOpenStart url; (!driver).Navigate().GoToUrl(url); Thread.Sleep(2000); doPageOpenReady url
-                              match (!driver).FindElementsByCssSelector(".b-tabs__tab-content_active .article-link") |> List.ofSeq with
-                                  | link :: _ -> doTabLinkClick(link.Text); link.Click(); Thread.Sleep(2000)
+                              browse url  
+                              match findSel ".b-tabs__tab-content_active .article-link" with
+                                  | link :: _ -> doTabLinkClick(link.Text); link.Click(); Thread.Sleep(1000)
                                   | [] -> ()
-                              doPageOpenStart url; (!driver).Navigate().GoToUrl(url); Thread.Sleep(2000); doPageOpenReady url
-                              match (!driver).FindElementsByCssSelector(".b-tabs__tab-content_active .b-link__read-more") |> List.ofSeq with
+                              browse url  
+                              match findSel ".b-tabs__tab-content_active .b-link__read-more" with
                                   | readMore :: _ -> doTabReadMoreClick(); readMore.Click(); Thread.Sleep(1000)
                                   | [] -> () 
                         | _ -> ()                    
                     | [] -> ()
-                match (!driver).FindElementsByClassName("sb-player") |> List.ofSeq with
+                match findSel ".sb-player" with
                     | video :: _ -> if video.Displayed then Thread.Sleep(90000)  
                     | [] -> ()
             with
-                | ex -> (!driver).Quit(); (!driver).Dispose(); driver := new FirefoxDriver(); 
+                | ex -> (cleanup >> reset)()
+
         urls |> List.iter runPageScenario
-        (!driver).Quit()
-        (!driver).Dispose()
+        cleanup()
